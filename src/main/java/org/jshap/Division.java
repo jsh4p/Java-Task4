dabef51844d5dd0575f4a25455c7202743383fbf
shap1kUS <j.shapovalov03@icloud.com>
2023-11-22 15:41:27 +0300

    +новая генерация ID

--- src/main/java/org/jshap/Division.java
+++ src/main/java/org/jshap/Division.java
@@ -1,10 +1,16 @@
 package org.jshap;
 
+import org.javatuples.Pair;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Сущность подразделение
  * @author jshap
  */
 public class Division {
+    private static Set<Pair<Integer, String>> names = new HashSet<>();
+    private static int IDCount = 1;
     private final int ID;
     private String name;
 
@@ -13,7 +19,24 @@ public class Division {
      * @param name имя подразделения
      */
     public Division(String name) {
-        ID = name.hashCode(); // некрутая реализация
+        boolean isContained = false;
+        int nextID = IDCount;
+
+        for (Pair<Integer, String> pair: names) {
+            if (name.equals(pair.getValue1())) {
+                nextID = pair.getValue0();
+                isContained = true;
+
+                break;
+            }
+        }
+
+        if (!isContained) {
+            names.add(Pair.with(nextID, name));
+            ++IDCount;
+        }
+
+        ID = nextID;
         this.name = name;
     }
 
